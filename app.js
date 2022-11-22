330036b4330e4e6e3ea8f7b64510cba78f3903fe
Yasir284 <lambawalay@gmail.com>
2022-11-22 11:13:54 +0530

    Added cors in app.js

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const app = express();
 require("./config/db").connect();
 const useroutes = require("./routes/userRoutes");
 
+const cors = require("cors");
+const corsOptions = {
+  credentials: true,
+  origin: "http://localhost:3000",
+  optionSucessStatus: true,
+};
+
 //Middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
